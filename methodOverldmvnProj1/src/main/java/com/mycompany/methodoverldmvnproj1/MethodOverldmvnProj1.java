515e75b92981bb371f6c796fad5aa660dca8d750
Saran <Saran@Saran-PC.attlocal.net>
2022-10-27 11:54:37 -0500

    comitting saran laptop code

---
+++ methodOverldmvnProj1/src/main/java/com/mycompany/methodoverldmvnproj1/MethodOverldmvnProj1.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.methodoverldmvnproj1;
+
+/**
+ *
+ * @author Saran
+ */
+public class MethodOverldmvnProj1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        for(int i =2; i<=8; i++)
+        {
+           double total = calculateInterest(10000,i);
+            System.out.println("Total Amount = "+String.format("%.2f", total)+" for Interest Rate "+i+"%");
+        
+        }
+        
+        boolean result = isPrime(5);
+        
+        System.out.println("Final Result is :"+result);
+        }
+    
+    
+    public static double calculateInterest(double amount, double interestRate)
+    {
+       return(amount*(interestRate/100));
+    }
+    
+    public static boolean isPrime(int prime){
+                
+        int i = 100;
+           
+         for(int j=2; j<=100 ; j++)
+         {
+             int remainder = i%j;
+         
+        // float remainder1 = i%3;
+         if (remainder >0 )
+         {
+         System.out.println(+i+" is a Prime Number");
+            return true;    
+         }
+         j++;
+         }
+             
+    
+          return false;
+    }
+}
+    
+    
+    
+
---
+++ methodOverldmvnProj1/src/main/java/com/mycompany/methodoverldmvnproj1/switchCase.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.methodoverldmvnproj1;
+
+/**
+ *
+ * @author Saran
+ */
+public class switchCase {
+    
+    public static void main(String[] args){
+        
+        char switch1 = 'f';
+        
+        switch(switch1){
+            case 'a':
+                System.out.println("This is case "+switch1);
+                break;
+            case 'b':case'c':case'd':case'e':
+                  System.out.println("This is case "+switch1);
+                break;   
+            default:
+                System.out.println("Not Found");
+        }
+         String switchh = "jANuarY" ;
+        switch(switchh.toUpperCase()){
+        
+            case "JANUARY":
+                    System.out.println("Month is :"+switchh.toUpperCase());
+                    break;
+            case  "FEBUARY":
+                System.out.println("Month is :"+switchh.toUpperCase());
+                break;
+                    
+            default:
+                System.out.println("Month Not Found");
+        }
+        int day = -1;
+        switchCaseTheWeek(day);
+        
+        }
+      public static void switchCaseTheWeek(int day)
+      {
+            switch(day){
+            
+                case 0:
+                    System.out.println("The day is Sunday");
+                    break;
+                case 1:
+                    System.out.println("The day is Moneday");
+                    break;
+                default:
+                    System.out.println("Day of the week not Found");
+            }
+            
+            
+      }  
+      
+                
+        
+                
+    }
+    
+
